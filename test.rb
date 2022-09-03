4592d72188ee3513ac79a6e04a865a34d7856ccf
Kasper Timm Hansen <kaspth@gmail.com>
2022-09-03 09:24:58 +0200

    Replace access with methods for a slight more ease

--- test.rb
+++ test.rb
@@ -102,9 +102,11 @@ module ProcessSip
   end
 end
 
-@git = ProcessSip.git
-def @git.commit_all(message)
+git = ProcessSip.git
+define_method(:git) { git }
+def git.commit_all(message)
   add "." and commit :m, message
 end
 
-@git_dir = @git.with(git_dir: __dir__)
+git_dir = git.with(git_dir: __dir__)
+define_method(:git_dir) { git_dir }
