f075c814f23d5a6f2da887518e1bff7ef8b40e78
Kasper Timm Hansen <kaspth@gmail.com>
2024-02-17 18:26:11 +0100

    I like this naming pattern way better

--- test/process_sip_test.rb
+++ test/process_sip_test.rb
@@ -2,7 +2,7 @@
 
 require "test_helper"
 
-class TestProcessSip < Minitest::Test
+class ProcessSipTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::ProcessSip::VERSION
   end
