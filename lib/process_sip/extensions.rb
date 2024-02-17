7276c2aabdece45a98b2a0e03bc82d6c4f0ccd99
Kasper Timm Hansen <kaspth@gmail.com>
2024-02-17 18:39:06 +0100

    Try some wild syntax why not

--- lib/process_sip/extensions.rb
+++ lib/process_sip/extensions.rb
@@ -1,21 +1,10 @@
 # frozen_string_literal: true
 
 module ProcessSip::Extensions
-  refine Symbol do
-    def dasherize
-      name.dasherize
-    end
-  end
-
-  refine String do
-    def dasherize
-      tr("_", "-")
-    end
-  end
+  refine(String) { def dasherize = tr("_", "-") }
+  refine(Symbol) { def dasherize = name.dasherize }
 
   refine Enumerable do
-    def index_with(default)
-      to_h { [ _1, default ] }
-    end
+    def index_with(default) = to_h { [ _1, default ] }
   end
 end
